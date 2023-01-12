1629788874c8ce17b4de7ca05a76f5ab7adb5cb8
Thato Khosa <TKhosa@SINGULAR.CO.ZA>
2023-01-12 13:36:22 +0200

    Authentication

--- 0x02-Session_authentication/api/v1/views/index (2).py
+++ 0x02-Session_authentication/api/v1/views/index (2).py
@@ -26,15 +26,19 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
-    """handler for - GET /api/v1/unauthorized
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def test_forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - Raise error
     """
-    abort(401)
+    return abort(403)
 
 
-@app_views.route('/forbidden/', methods=['GET'], strict_slashes=False)
-def forbidden() -> str:
-    """handler for - GET /api/v1/forbidden
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def test_unathourized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - Raise error
     """
-    abort(403)
+    return abort(401)
